5c1db894c6f57561d8e4ceeab42edb65b402e1eb
NguyenAnh55 <s3924711@rmit.edu.vn>
2024-01-13 11:10:47 +0700

    add setter for Ride class, add constructor based on DocumentSnapshot

--- app/src/main/java/com/example/assignment3/Class/Ride.java
+++ app/src/main/java/com/example/assignment3/Class/Ride.java
@@ -1,5 +1,6 @@
 package com.example.assignment3.Class;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.GeoPoint;
 
 import java.io.Serializable;
@@ -14,6 +15,25 @@ public class Ride implements Serializable {
     private String uidDriver;
 
     public Ride(){}
+
+    public Ride(GeoPoint driverLocation, GeoPoint dropPoint, GeoPoint pickPoint, String status,
+                String uidCustomer, String uidDriver) {
+        DriverLocation = driverLocation;
+        DropPoint = dropPoint;
+        PickPoint = pickPoint;
+        this.status = status;
+        this.uidCustomer = uidCustomer;
+        this.uidDriver = uidDriver;
+    }
+
+    public Ride (DocumentSnapshot document){
+        this.uidDriver = document.getString("uidCustomer");
+        this.uidCustomer = document.getString("uidCustomer");
+        this.DriverLocation = document.getGeoPoint("DriverLocation");
+        this.DropPoint = document.getGeoPoint("DropPoint");
+        this.PickPoint = document.getGeoPoint("PickPoint");
+        this.status = document.getString("status");
+    }
 
     public GeoPoint getDriverLocation() {
         return DriverLocation;
@@ -37,6 +57,30 @@ public class Ride implements Serializable {
 
     public String getUidDriver() {
         return uidDriver;
+    }
+
+    public void setDriverLocation(GeoPoint driverLocation) {
+        DriverLocation = driverLocation;
+    }
+
+    public void setDropPoint(GeoPoint dropPoint) {
+        DropPoint = dropPoint;
+    }
+
+    public void setPickPoint(GeoPoint pickPoint) {
+        PickPoint = pickPoint;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setUidCustomer(String uidCustomer) {
+        this.uidCustomer = uidCustomer;
+    }
+
+    public void setUidDriver(String uidDriver) {
+        this.uidDriver = uidDriver;
     }
 }
 
