2a5b741de3d18bcb4951d21b4c870887fe9b5b2b
Tran Dai Phuc <S3924715@rmit.edu.vn>
2024-01-18 19:23:38 +0700

    Add function to display old information in edit profile.

--- app/src/main/java/com/example/assignment3/Customer/EditProfileCustomerActivity.java
+++ app/src/main/java/com/example/assignment3/Customer/EditProfileCustomerActivity.java
@@ -14,12 +14,14 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.assignment3.Class.Customer;
 import com.example.assignment3.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
@@ -27,6 +29,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -74,24 +77,62 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
         emailInput = (EditText) findViewById(R.id.emailInput);
         phoneInput = (EditText) findViewById(R.id.phoneInput);
         addressInput = (EditText) findViewById(R.id.addressInput);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
-        updateButton = (Button) findViewById(R.id.updateButton);
-        deleteButton = (Button) findViewById(R.id.deleteButton);
+        //passwordInput = (EditText) findViewById(R.id.passwordInput);
         spinnerGender = findViewById(R.id.spinnerGender);
 
-         // Set interaction for update button
+        updateButton = (Button) findViewById(R.id.updateButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+
+        // Set values for gender spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_types, R.layout.spinner_item_text);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter(adapter);
+
+        // Set old information of the customer
+        database.collection("Customers").document(uid).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Customer customer = document.toObject(Customer.class);
+                    assert customer != null;
+
+                    nameInput.setText(customer.getName());
+                    emailInput.setText(customer.getEmail());
+                    phoneInput.setText(customer.getPhone());
+                    addressInput.setText(customer.getAddress());
+
+                    // Check if the gender field is null or empty
+                    if (customer.getGender() != null && !customer.getGender().isEmpty()) {
+                        int position = adapter.getPosition(customer.getGender());
+                        spinnerGender.setSelection(position);
+                    } else {
+                        // Set a default value for the spinner
+                        spinnerGender.setSelection(
+                                0); // Change this to the position of your default value
+                    }
+
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
+            }
+        });
+
+        // Set interaction for update button
         updateButton.setOnClickListener(v -> {
-                // Get all texts from edit texts
-                String name = nameInput.getText().toString();
-                String email = emailInput.getText().toString();
-                String phone = phoneInput.getText().toString();
-                String address = addressInput.getText().toString();
-                String password = passwordInput.getText().toString();
+            // Get all texts from edit texts
+            String name = nameInput.getText().toString();
+            String email = emailInput.getText().toString();
+            String phone = phoneInput.getText().toString();
+            String address = addressInput.getText().toString();
+            //String password = passwordInput.getText().toString();
             spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
-                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                public void onItemSelected(AdapterView<?> parentView, View selectedItemView,
+                                           int position, long id) {
                     // Retrieve the selected gender
-                    selectedGender = getResources().getStringArray(R.array.gender_options)[position];
+                    selectedGender = getResources().getStringArray(
+                            R.array.gender_options)[position];
                 }
 
                 @Override
@@ -100,71 +141,42 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
                 }
             });
 
-                boolean isValid = true;
-                if (email.isEmpty()) {
-                    emailInput.setError("Invalid email");
-                    isValid = false;
-                }
-                if (password.isEmpty()) {
-                    passwordInput.setError("Invalid password");
-                    isValid = false;
-                }
-                if (name.isEmpty()) {
-                    nameInput.setError("Invalid name");
-                    isValid = false;
-                }
-                if (phone.isEmpty()) {
-                    phoneInput.setError("Invalid phone number");
-                    isValid = false;
-                }
-                if (address.isEmpty()) {
-                    addressInput.setError("Invalid ward");
-                    isValid = false;
-                }
+            // Create an alert dialog to confirm
+            new AlertDialog.Builder(this).setTitle("Confirm save")
+                    .setMessage("Are you sure you want to save these changes?")
+                    .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                        user.verifyBeforeUpdateEmail(email)
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        Log.d(TAG, "User email address updated.");
+                                    } else {
+                                        Log.w(TAG, "Failed to update email.",
+                                                task1.getException());
+                                    }
+                                });
+//                            user.updatePassword(password)
+//                                    .addOnCompleteListener(task2 -> {
+//                                        if (task2.isSuccessful()) {
+//                                            Log.d(TAG, "User password updated.");
+//                                            Toast.makeText(EditProfileCustomerActivity.this,
+//                                                    "User password updated.",
+//                                                    Toast.LENGTH_SHORT).show();
+//                                        } else {
+//                                            Log.w(TAG, "Failed to update password.",
+//                                                    task2.getException());
+//                                            Toast.makeText(EditProfileCustomerActivity.this,
+//                                                    "Failed to update password",
+//                                                    Toast.LENGTH_SHORT).show();
+//                                        }
+//                                    });
+                        update.put("name", name);
+                        update.put("phone", phone);
+                        update.put("address", address);
+                        update.put("gender", selectedGender);
+                        uploadImageToStorageAndFirestore(imageUri, customerRef);
+                    }).setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert).show();
 
-            if (isValid) {
-                // Create an alert dialog to confirm
-                new AlertDialog.Builder(this).setTitle("Confirm save")
-                        .setMessage("Are you sure you want to save these changes?")
-                        .setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                            user.verifyBeforeUpdateEmail(email)
-                                    .addOnCompleteListener(task1 -> {
-                                        if (task1.isSuccessful()) {
-                                            Log.d(TAG, "User email address updated.");
-                                            Toast.makeText(EditProfileCustomerActivity.this,
-                                                    "User email address updated.",
-                                                    Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            Log.w(TAG, "Failed to update email.",
-                                                    task1.getException());
-                                            Toast.makeText(EditProfileCustomerActivity.this,
-                                                    "Failed to update email.",
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                            user.updatePassword(password)
-                                    .addOnCompleteListener(task2 -> {
-                                        if (task2.isSuccessful()) {
-                                            Log.d(TAG, "User password updated.");
-                                            Toast.makeText(EditProfileCustomerActivity.this,
-                                                    "User password updated.",
-                                                    Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            Log.w(TAG, "Failed to update password.",
-                                                    task2.getException());
-                                            Toast.makeText(EditProfileCustomerActivity.this,
-                                                    "Failed to update password",
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                            update.put("name", name);
-                            update.put("phone", phone);
-                            update.put("address", address);
-                            update.put("gender", selectedGender);
-                            uploadImageToStorageAndFirestore(imageUri, customerRef);
-                        }).setNegativeButton(android.R.string.no, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert).show();
-            }
         });
 
         // Set interaction for delete button
@@ -197,14 +209,17 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
                                 profilePicture.setImageDrawable(null); // Clear existing image
                                 profilePicture.setImageURI(imageUri);
                             } else {
-                                Toast.makeText(this, "Image URI is null!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(this, "Image URI is null!", Toast.LENGTH_SHORT)
+                                        .show();
                             }
                         } else {
-                            Toast.makeText(this, "Image selection canceled", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this, "Image selection canceled", Toast.LENGTH_SHORT)
+                                    .show();
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
-                        Toast.makeText(this, "Error handling image selection result", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Error handling image selection result",
+                                Toast.LENGTH_SHORT).show();
                     }
                 });
 
@@ -213,7 +228,11 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
             intent.setType("image/*");
             launcher.launch(Intent.createChooser(intent, "Choose an Image"));
         });
+
+        ImageView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> finish());
     }
+
     private void uploadImageToStorageAndFirestore(Uri imageUri, DocumentReference customerRef) {
         // Initialize Firebase Storage
         FirebaseStorage storage = FirebaseStorage.getInstance();
@@ -232,7 +251,8 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
                 uploadNewImage(imageUri, imageRef, customerRef);
             }).addOnFailureListener(e -> {
                 // Handle failure to delete the older image
-                Toast.makeText(EditProfileCustomerActivity.this, "Failed to delete the older image", Toast.LENGTH_SHORT).show();
+                Toast.makeText(EditProfileCustomerActivity.this, "Failed to delete the older image",
+                        Toast.LENGTH_SHORT).show();
             });
         }).addOnFailureListener(e -> {
             // No older image exists, upload the new image directly
@@ -244,15 +264,18 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
         customerRef.update("profileImageUri", newImageUri)
                 .addOnSuccessListener(aVoid -> {
                     // Update successful
-                    Toast.makeText(EditProfileCustomerActivity.this, "Image URI updated in Firestore", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditProfileCustomerActivity.this,
+                            "Image URI updated in Firestore", Toast.LENGTH_SHORT).show();
                 })
                 .addOnFailureListener(e -> {
                     // Handle failure
-                    Toast.makeText(EditProfileCustomerActivity.this, "Failed to update image URI in Firestore", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditProfileCustomerActivity.this,
+                            "Failed to update image URI in Firestore", Toast.LENGTH_SHORT).show();
                 });
     }
 
-    private void uploadNewImage(Uri imageUri, StorageReference imageRef, DocumentReference customerRef) {
+    private void uploadNewImage(Uri imageUri, StorageReference imageRef,
+                                DocumentReference customerRef) {
         // Upload the new image
         imageRef.putFile(imageUri)
                 .addOnSuccessListener(taskSnapshot -> {
@@ -264,12 +287,14 @@ public class EditProfileCustomerActivity extends AppCompatActivity {
                             })
                             .addOnFailureListener(e -> {
                                 // Handle failure to get download URL
-                                Toast.makeText(EditProfileCustomerActivity.this, "Failed to get download URL", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(EditProfileCustomerActivity.this,
+                                        "Failed to get download URL", Toast.LENGTH_SHORT).show();
                             });
                 })
                 .addOnFailureListener(e -> {
                     // Handle failure to upload image
-                    Toast.makeText(EditProfileCustomerActivity.this, "Failed to upload image", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditProfileCustomerActivity.this, "Failed to upload image",
+                            Toast.LENGTH_SHORT).show();
                 });
     }
 }
