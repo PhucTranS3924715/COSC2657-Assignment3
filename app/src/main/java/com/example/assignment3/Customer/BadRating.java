b830585a62482939a2fb4e9f95fc68f491384c3c
NguyenAnh55 <s3924711@rmit.edu.vn>
2024-01-21 15:17:49 +0700

    implement submit button for bad rating and good rating modify UI for good rating

--- app/src/main/java/com/example/assignment3/Customer/BadRating.java
+++ app/src/main/java/com/example/assignment3/Customer/BadRating.java
@@ -1,8 +1,12 @@
 package com.example.assignment3.Customer;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.assignment3.R;
 
@@ -11,5 +15,16 @@ public class BadRating extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create activity launcher
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {});
+
+        Button submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(v -> {
+            Intent intent1 = new Intent(this, HomeFragment.class);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear all activities in the stack
+            launcher.launch(intent1);
+        });
     }
 }
--- app/src/main/java/com/example/assignment3/Customer/GoodRating.java
+++ app/src/main/java/com/example/assignment3/Customer/GoodRating.java
@@ -1,15 +1,40 @@
 package com.example.assignment3.Customer;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.assignment3.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class GoodRating extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create activity launcher
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {});
+
+        Button doneButton = findViewById(R.id.DoneButton);
+        TextView skipButton = findViewById(R.id.textView8);
+
+        doneButton.setOnClickListener(v -> {
+            Intent intent1 = new Intent(this, HomeFragment.class);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear all activities in the stack
+            launcher.launch(intent1);
+        });
+
+        skipButton.setOnClickListener(v -> {
+            Intent intent1 = new Intent(this, HomeFragment.class);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear all activities in the stack
+            launcher.launch(intent1);
+        });
     }
 }
